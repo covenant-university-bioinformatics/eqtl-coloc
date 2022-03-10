f50fb6d7b71296f653b97e7d899f8028f7e36528
yagoubali <yagoubali@gmail.com>
2022-03-10 19:25:11 -0300

    updating eqtl files names

--- scripts/coloc.R
+++ scripts/coloc.R
@@ -21,6 +21,7 @@ library('coloc')
 #library("locuscomparer")
 
 #eqtl <- read.table(gzfile(paste0(dbdir,"/",eqtl_file)),sep="\t", header=T, as.is=T);
+eqtl_file=paste0(eqtl_file,".v8.signif_variant_gene_pairs.txt")
 eqtl <- read.table(eqtl_file,sep="", header=T, as.is=T);
 
 #hg19_lookup_table= paste0(dbdir,"/","GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.lookup_table.txt.gz")
--- scripts/coloc.sh
+++ scripts/coloc.sh
@@ -10,7 +10,12 @@
 #RUN R -e "devtools::install_github('boxiangliu/locuscomparer')"
 #RUN R -e "install.packages(c('coloc'),dependencies=TRUE,repos='http://cran.rstudio.com/')"
 set -x;
-bindir="/mnt/d/eqtl-coloc"
+#development
+bindir="/local/datasets/eqtlcoloc"
+
+#production
+#bindir="/mnt/d/eqtl-coloc"
+
 dbdir="${bindir}/coloc_eqtl_files"
 gwas_summary=$1
 outdir=$2
@@ -35,3 +40,55 @@ Rscript --vanilla ${bindir}/coloc.R ${gwas_summary}  ${outdir} ${dbdir}/${GTEX8t
 
 
 #./coloc.sh UKB_bv_height_rsids_0.05.txt outdir2 Adipose_Subcutaneous.v8.signif_variant_gene_pairs.txt 1e-04 1e-04 1e-05 cc 0.33
+
+
+######Tissues
+# Adipose_Subcutaneous
+# Adipose_Visceral_Omentum
+# Adrenal_Gland
+# Artery_Aorta
+# Artery_Coronary
+# Artery_Tibial
+# Brain_Amygdala
+# Brain_Anterior_cingulate_cortex_BA24
+# Brain_Caudate_basal_ganglia
+# Brain_Cerebellar_Hemisphere
+# Brain_Cerebellum
+# Brain_Cortex
+# Brain_Frontal_Cortex_BA9
+# Brain_Hippocampus
+# Brain_Hypothalamus
+# Brain_Nucleus_accumbens_basal_ganglia
+# Brain_Putamen_basal_ganglia
+# Brain_Spinal_cord_cervical_c-1
+# Brain_Substantia_nigra
+# Breast_Mammary_Tissue
+# Cells_Cultured_fibroblasts
+# Cells_EBV-transformed_lymphocytes
+# Colon_Sigmoid
+# Colon_Transverse
+# Esophagus_Gastroesophageal_Junction
+# Esophagus_Mucosa
+# Esophagus_Muscularis
+# Heart_Atrial_Appendage
+# Heart_Left_Ventricle
+# Kidney_Cortex
+# Liver
+# Lung
+# Minor_Salivary_Gland
+# Muscle_Skeletal
+# Nerve_Tibial
+# Ovary
+# Pancreas
+# Pituitary
+# Prostate
+# Skin_Not_Sun_Exposed_Suprapubic
+# Skin_Sun_Exposed_Lower_leg
+# Small_Intestine_Terminal_Ileum
+# Spleen
+# Stomach
+# Testis
+# Thyroid
+# Uterus
+# Vagina
+# Whole_Blood
